0c6dce0f8c7ff8be35ff735d8a428c2bb2798ee8
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-10-08 18:28:08 -0300

    fix (route-content): adicionando logica de ID para pegar valores do dataFake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake: {
+  id: string;
+  photo: string;
+  title: string;
+  description: string;
+}[] = [
+  {
+    id: '0',
+    photo:
+      'https://upload.wikimedia.org/wikipedia/pt/5/5a/Metal_Gear_Rising_capa.png',
+    title: 'Metal Gear Risign',
+    description: 'The best game evar',
+  },
+  {
+    id: '1',
+    photo:
+      'https://assetsio.reedpopcdn.com/jetstreamsam.jpg?width=1200&height=630&fit=crop&enable=upscale&auto=webp',
+    title: 'Jetstream Sam',
+    description: 'brabone',
+  },
+  {
+    id: '2',
+    photo:
+      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/34a64499-31e2-4cad-82be-d83d7504ad51/dan17ek-d7c9a06f-614b-4573-81a6-40d71b937e86.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM0YTY0NDk5LTMxZTItNGNhZC04MmJlLWQ4M2Q3NTA0YWQ1MVwvZGFuMTdlay1kN2M5YTA2Zi02MTRiLTQ1NzMtODFhNi00MGQ3MWI5MzdlODYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.6RwXb1wsqcbRk5o9cLuXths1y8MhXSlwRNc9MXXvPVU',
+    title: 'Monsoon',
+    description: 'battle epico',
+  },
+  {
+    id: '3',
+    photo: 'https://i.ytimg.com/vi/GoDDYceR-TQ/maxresdefault.jpg',
+    title: 'Mistral',
+    description: 'stranger reing me',
+  },
+];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -15,6 +16,7 @@ export class ContentComponent {
   @Input()
   cardDescription: string =
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, omnis? Atque tempora facilis eos provident nesciunt saepe, veniam doloribus. Unde officiis obcaecati a atque nam id animi, quo ad facere?';
+  private id: string | null = '0'
 
   constructor(
     private route: ActivatedRoute
@@ -24,7 +26,17 @@ export class ContentComponent {
 // pegar o valor do id
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get('id'))
+      this.id = value.get('id')
+      // console.log(value.get('id'))
     )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null){
+    const result = dataFake.filter(article => article.id === id)[0];
+
+    this.cardTitle = result.title;
+    this.cardDescription = result.description;
+    this.photoCover = result.photo;
   }
 }
