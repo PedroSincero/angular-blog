e2e0e093f75be911da2a4a1adc51fd43f992f5b5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-10-08 16:58:22 -0300

    fix (big-card): adicionando componenticação ao bigcard

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string =
+    'https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png';
+  @Input()
+  cardTitle: string = 'PLACE HOLDER TITLE';
+  @Input()
+  cardDescription: string =
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, omnis? Atque tempora facilis eos provident nesciunt saepe, veniam doloribus. Unde officiis obcaecati a atque nam id animi, quo ad facere?';
 
+  constructor() {}
 }
