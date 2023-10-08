80ddf41a88f643a40bd2e7a329ce04dbb2d9c1ad
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-10-08 17:09:33 -0300

    fix (small-card): finalizando componentização do smallCard

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
-
+  @Input()
+  photoCover: string =
+    'https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png';
+  @Input()
+  titleCard: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.';
 }
